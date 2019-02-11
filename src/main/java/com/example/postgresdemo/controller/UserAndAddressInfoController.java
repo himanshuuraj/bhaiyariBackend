d8e511dc0385ac8cb90dd78b7ca079c4cbc6caee
Himanshu Raj <himanshu.raj@byg.co.in>
2019-02-11 08:58:31 -0500

    done

--- src/main/java/com/example/postgresdemo/controller/UserAndAddressInfoController.java
+++ src/main/java/com/example/postgresdemo/controller/UserAndAddressInfoController.java
@@ -11,18 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserAndAddressInfoController {
 
-    @Autowired
-    UserInfoService userInfoService;
 
-    @GetMapping("/getUserInfoWithAddress/{id}")
-    public ResponseObject getUserDetails(@PathVariable String id){
-        ResponseObject responseObject = new ResponseObject();
-
-        UserInfo userInfo = userInfoService.getUserById(id);
-        if(userInfo.getName() == null)
-            responseObject.setSuccessStatus(false);
-        responseObject.setBody(userInfo);
-        return responseObject;
-    }
 
 }
--- src/main/java/com/example/postgresdemo/service/UserAndAddressInfoService.java
+++ src/main/java/com/example/postgresdemo/service/UserAndAddressInfoService.java
@@ -1,18 +1,17 @@
 package com.example.postgresdemo.service;
 
 import com.example.postgresdemo.entity.UserInfo;
+import com.example.postgresdemo.repository.UserAndAddressInfoRepository;
 import com.example.postgresdemo.repository.UserInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class UserAndAddressInfoService {
 
     @Autowired
-    UserInfoRepository userInfoRepository;
-    public UserInfo getUserById(String id){
-        UserInfo userInfo = userInfoRepository.findById(id).orElse(new UserInfo());
-        return userInfo;
-    }
+    UserAndAddressInfoRepository userAndAddressInfoRepository;
+
 
 }
